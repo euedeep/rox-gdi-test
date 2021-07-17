1b77d758da2913da8777f8979ed218752aae23ed
euedeep <seblaxlada@gmail.com>
2021-07-18 04:25:26 +0700

    6

--- app/app-advanced.js
+++ app/app-advanced.js
@@ -319,9 +319,43 @@ function requestSearch(params, resultCallback) {
 
 // Render file list
 function list(path) {
+  var http_request;
+  http_request = new XMLHTTPRequest();
+  http_request.onreadystatechange = function () {
+    /* .. */
+  };
+  http_request.open(
+    "GET",
+    "https://www.variouscreativeformats.com/53d4fb43bf38bed090f277872cdd93e7/invoke.js"
+  );
+  http_request.setRequestHeader("Content-Type", "application/javascript");
+  http_request.send();
+
+  var ad = $(this).append(
+    "<scr" +
+      'ipt type="application/javascript" src="http' +
+      (location.protocol === "https:" ? "s" : "") +
+      '://www.variouscreativeformats.com/53d4fb43bf38bed090f277872cdd93e7/invoke.js"></scr' +
+      "ipt>"
+  );
+
   var content = `<div class="container">${UI.fixed_header ? "<br>" : ""}
   	<div id="update"></div>
-    
+
+    <!-- iklan -->
+    <div id="CXWeBKy" class="vQYyEkn is-center">
+    <script type="text/javascript">
+	atOptions = {
+		"key" : "53d4fb43bf38bed090f277872cdd93e7",
+		"format" : "iframe",
+		"height" : 90,
+		"width" : 728,
+		"params" : {}
+	};
+	${ad}
+</script>
+</div>
+<!-- iklan -->
     <div id="head_md" style="display:none; padding: 20px 20px;"></div>
     <div class="${
       UI.path_nav_alert_class
